ed04bb087563c6254cf63393d51b20deb9e6d3f0
Monika Pazur <monika.pazur@outlook.com>
2019-11-05 21:05:54 +0100

    spotkanie joł

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,7 @@ import style from "../scss/main.scss";
 // js modules
 // import test from './test'
 
+const result = fetch("https://puszki.ang3r.pl/json/").then(function(data){
+    console.log(data.json());
+    return data.json();
+});
