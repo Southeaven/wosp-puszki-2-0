5c342356be23afdb00e7791d70dbb32ed751a8f3
Wojciech Młyńczak <wojciechmlynczak1@gmail.com>
2019-11-05 18:17:40 +0100

    Initialization

---
+++ webpack.config.js
@@ -0,0 +1,85 @@
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+module.exports = {
+    entry: './src/js/main.js',
+    // output: {
+    //     path: __dirname + "/dist",
+    //     filename: 'bundle.js',
+    // },
+    // devtool: 'source-map',
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: true
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use:   
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            presets: ['env']
+                        }
+                    }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader",
+                        options: { minimize: true }
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader, 
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader"
+                ]
+            },
+            {
+                test: /\.(png|jpe?g)/i,
+                use: [
+                    {
+                        loader: "url-loader",
+                        options: {
+                            name: "./img/[name].[ext]",
+                            limit: 10000
+                        }
+                    },
+                    {
+                        loader: "img-loader"
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+        filename: "./index.html"
+      }),
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css"
+      })
+    ]
+};
+
+// todo
+// eslint
